50cf9f211f9e75b3723c2a2660782de852ae7daf
PleYa <aaiaia.gg@gmail.com>
2023-03-27 08:25:21 +0900

    updated sequence 2-10, added view question detail and exceptions not found entity

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,10 +1,13 @@
 package com.mysite.sbb.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+
+import com.mysite.sbb.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +18,16 @@ public class QuestionService {
 	public List<Question> getList(){
 		System.out.println("called getList(), in " + this.getClass().getName());
 		return this.questionRepository.findAll();
+	}
+
+	public Question getQuestion(Integer id){
+		Optional<Question> oq = this.questionRepository.findById(id);
+		if(oq.isPresent()) {
+			return oq.get();
+		}
+		else
+		{
+			throw new DataNotFoundException("question not found");
+		}
 	}
 }
