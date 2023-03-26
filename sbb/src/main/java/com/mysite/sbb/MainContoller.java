df72d7cf4d0b6e44be4c0b49aa61b431080091da
PleYa <aaiaia.gg@gmail.com>
2023-03-27 07:19:10 +0900

    updated sequence 2-08, root url redirect

--- sbb/src/main/java/com/mysite/sbb/MainContoller.java
+++ sbb/src/main/java/com/mysite/sbb/MainContoller.java
@@ -13,4 +13,10 @@ public class MainContoller {
 		//System.out.println("index");
 		return "안녕하세요 sbb에 오신것을 환영합니다.";
 	}
+
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/question/list";	//URL로 리다이렉트 (리다이렉트는 완전히 새로운 URL로 요청이 된다.) 
+		//return "forward:/question/list";	//URL로 포워드 (포워드는 기존 요청 값들이 유지된 상태로 URL이 전환된다.)
+	}
 }
