722489ccf157149906a64bf8f83d12bdea2b28a5
PleYa <aaiaia.gg@gmail.com>
2023-03-27 06:37:43 +0900

    [WIP]Updated sequence ~2-07, fixed QuestionContoller.java to return string

--- sbb/src/main/java/com/mysite/sbb/question/QuestionContoller.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionContoller.java
@@ -1,5 +1,15 @@
 package com.mysite.sbb.question;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class QuestionContoller {
 
+	@GetMapping("/question/list")
+	@ResponseBody
+	public String list() {
+		return "question list";
+	}
 }
