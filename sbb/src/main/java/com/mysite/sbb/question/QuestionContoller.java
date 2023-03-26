73cb235d6b3308066d57a9e07ee49c4bab534066
PleYa <aaiaia.gg@gmail.com>
2023-03-27 06:39:55 +0900

    [WIP]Updated sequence ~2-07, fixed QuestionContoller.java to return templete

--- sbb/src/main/java/com/mysite/sbb/question/QuestionContoller.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionContoller.java
@@ -2,14 +2,14 @@ package com.mysite.sbb.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuestionContoller {
 
 	@GetMapping("/question/list")
-	@ResponseBody
+	//@ResponseBody
 	public String list() {
-		return "question list";
+		return "question_list";
 	}
 }
