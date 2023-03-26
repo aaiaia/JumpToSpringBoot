c78bb2637b2696d829da5f3e22182e603f8c7163
PleYa <aaiaia.gg@gmail.com>
2023-03-27 01:39:14 +0900

    [WIP]Updated sequence ~2-05, find answer by id

--- sbb/src/test/java/com/mysite/sbb/Test010FindAnswerById.java
+++ sbb/src/test/java/com/mysite/sbb/Test010FindAnswerById.java
@@ -1,5 +1,38 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class Test010FindAnswerById {
 
+	@Autowired
+	private AnswerRepository answerRepository;
+	
+	@Test
+	void testFindAnswer() {
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		if(oa.isPresent()) {
+			assertTrue(oa.isPresent());
+			Answer a = oa.get();
+			Question q = a.getQuestion();
+
+			System.out.println(q.getId() +  ":" + q.getCreateDate());
+			System.out.println("subject: " + q.getSubject());
+			System.out.println("content: " + q.getContent());
+
+			System.out.println(a.getId() +  ":" + a.getCreateDate());
+			System.out.println("content: " + a.getContent());
+		}
+		else {
+			assertFalse(oa.isPresent());
+		}
+	}
 }
