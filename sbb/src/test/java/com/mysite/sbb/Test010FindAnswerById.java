583b15ee92aa8c0baabfaefa0742f595abe30e03
PleYa <aaiaia.gg@gmail.com>
2023-03-27 02:03:50 +0900

    blocked not used package

--- sbb/src/test/java/com/mysite/sbb/Test010FindAnswerById.java
+++ sbb/src/test/java/com/mysite/sbb/Test010FindAnswerById.java
@@ -1,6 +1,6 @@
 package com.mysite.sbb;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
