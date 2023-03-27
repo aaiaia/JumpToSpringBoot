f2ff7dd3e885ee36ef6e5f38eca2976fef099a09
PleYa <aaiaia.gg@gmail.com>
2023-03-27 18:51:39 +0900

    updated sequence 2-11, create(reply) answer @ question

---
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,27 @@
+package com.mysite.sbb.answer;
+
+import java.time.LocalDateTime;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.question.Question;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+	private final AnswerRepository answerRepository;
+
+	public void create(Question question, String content) {
+        /* Just Debugging */
+		System.out.println("called: " + new Object(){}.getClass().getEnclosingMethod().getName() + ", in " + this.getClass().getName());
+        /* Make Answer() and set Object values */
+		Answer answer = new Answer();
+		answer.setCreateDate(LocalDateTime.now());
+		answer.setContent(content);
+		answer.setQuestion(question);
+        /* Saving answer using answerRepository(), kind of interface */
+		answerRepository.save(answer);
+	}
+}
