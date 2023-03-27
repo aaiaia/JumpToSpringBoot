92b2e9fc2882a7bb3ed2ce437f2527e69a5e1ed1
PleYa <aaiaia.gg@gmail.com>
2023-03-28 03:12:17 +0900

    [WIP]updated sequnce 2-15, implementated QuestionForm to check validation

--- sbb/src/main/java/com/mysite/sbb/question/QuestionForm.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -1,5 +1,18 @@
 package com.mysite.sbb.question;
 
-public class QuestionForm {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
+import lombok.Setter;
+import lombok.Getter;
+
+@Setter
+@Getter
+public class QuestionForm {
+	@NotEmpty(message="제목을 필수항목입니다.(최대 200자)")
+	@Size(max=200)
+	private String subject;
+
+	@NotEmpty(message="내용은 필수할목입니다.")
+	private String Content;
 }
