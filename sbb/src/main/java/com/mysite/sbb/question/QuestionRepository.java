8691b3e39d3c7fe8974769eef0c6216c441994bb
PleYa <aaiaia.gg@gmail.com>
2023-03-28 18:30:43 +0900

    [WIP]3-02 Paging, added iterface findAll() with pageable in questionRepository

--- sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /* Question 오브젝트트의 처리를 위해 JpaRepository 상속 및 제네릭으로, 다음엔 Primary Key인 Id 의 속성을 넣음 */
@@ -9,4 +11,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>{
 	List<Question> findBySubject(String subject);
 	List<Question> findBySubjectAndContent(String subject, String content);
 	List<Question> findBySubjectLike(String subject);
+	Page<Question> findAll(Pageable pageable);
 }
