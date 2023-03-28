84f5faeb923f12349bd51892aaaa1377687ae5be
PleYa <aaiaia.gg@gmail.com>
2023-03-28 18:09:00 +0900

    [WIP]3-02 Paging, added Test Data

--- sbb/src/test/java/com/mysite/sbb/Test012_3_02_AddTestData_300.java
+++ sbb/src/test/java/com/mysite/sbb/Test012_3_02_AddTestData_300.java
@@ -1,5 +1,23 @@
 package com.mysite.sbb;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.question.QuestionService;
+
+@SpringBootTest
 public class Test012_3_02_AddTestData_300 {
 
+	@Autowired
+	private QuestionService questionService;
+
+	@Test
+	void testJpaAddTestDataForPaging() {
+		for(int i = 0; i < 100; i++) {
+			String subject = String.format("테스트 제목입니다:[%03d]", i);
+			String content = String.format("내용무:[%03d]", i);
+			this.questionService.create(subject, content);
+		}
+	}
 }
