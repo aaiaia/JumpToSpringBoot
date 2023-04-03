f6ceb69dac5a3aeaf3ae14013883a79d290429fd
PleYa <aaiaia.gg@gmail.com>
2023-04-03 17:33:36 +0900

    [WIP] 3.06, implementated SiteUserRepository

--- sbb/src/main/java/com/mysite/sbb/user/SiteUserRepository.java
+++ sbb/src/main/java/com/mysite/sbb/user/SiteUserRepository.java
@@ -1,5 +1,9 @@
 package com.mysite.sbb.user;
 
-public interface SiteUserRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SiteUserRepository extends JpaRepository<SiteUser, Long>{
 
 }
