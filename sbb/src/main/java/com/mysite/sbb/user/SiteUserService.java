9a5dd7dc4966a8cc49b4c461d7d7c426c036b54a
PleYa <aaiaia.gg@gmail.com>
2023-04-03 17:44:31 +0900

    [WIP] 3.06, implementated SiteUserService

--- sbb/src/main/java/com/mysite/sbb/user/SiteUserService.java
+++ sbb/src/main/java/com/mysite/sbb/user/SiteUserService.java
@@ -1,5 +1,23 @@
 package com.mysite.sbb.user;
 
-public class SiteUserService {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class SiteUserService {
+	private final SiteUserRepository siteUserRepository;
+
+	public SiteUser create(String username, String password, String email) {
+		BCryptPasswordEncoder passwdEncoder = new BCryptPasswordEncoder();
+		SiteUser user = new SiteUser();
+		user.setUsername(username);
+		user.setPassword(passwdEncoder.encode(password));
+		user.setEmail(email);
+
+		siteUserRepository.save(user);
+		return user;
+	}
 }
