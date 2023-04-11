3ce0e9f5e3e1ddd866cbab8d4e1d345dacab0390
PleYa <aaiaia.gg@gmail.com>
2023-04-12 03:20:40 +0900

    [WIP] 3.08, added getUser() @ SiteUserService

--- sbb/src/main/java/com/mysite/sbb/user/SiteUserService.java
+++ sbb/src/main/java/com/mysite/sbb/user/SiteUserService.java
@@ -1,9 +1,14 @@
 package com.mysite.sbb.user;
 
+import java.util.Optional;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+
+/* for common excepion in this projects*/
+import com.mysite.sbb.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -19,5 +24,14 @@ public class SiteUserService {
 
 		siteUserRepository.save(user);
 		return user;
+	}
+
+	public SiteUser getUser(String username) {
+		Optional<SiteUser> siteUser = this.siteUserRepository.findByUsername(username);
+		if(siteUser.isPresent()) {
+			return siteUser.get();
+		} else {
+			throw new DataNotFoundException("siteuser not found");
+		}
 	}
 }
