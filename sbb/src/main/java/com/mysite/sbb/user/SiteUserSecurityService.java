280733120b06bf36a5292ee93f7bac40fd5ff879
PleYa <aaiaia.gg@gmail.com>
2023-04-12 00:33:49 +0900

    [WIP] 3.07, implementated SiteUserSecurityService.java (1)

--- sbb/src/main/java/com/mysite/sbb/user/SiteUserSecurityService.java
+++ sbb/src/main/java/com/mysite/sbb/user/SiteUserSecurityService.java
@@ -1,5 +1,21 @@
 package com.mysite.sbb.user;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class SiteUserSecurityService {
 
 }
