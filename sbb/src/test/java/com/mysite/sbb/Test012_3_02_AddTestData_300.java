4faf4912472dd280bcc78b0293cf724e06ee20e3
PleYa <aaiaia.gg@gmail.com>
2023-04-12 03:41:23 +0900

    [WIP] 3.08, blocked questionService.create(), @ test012_3_02_AddTestData_300.java

--- sbb/src/test/java/com/mysite/sbb/Test012_3_02_AddTestData_300.java
+++ sbb/src/test/java/com/mysite/sbb/Test012_3_02_AddTestData_300.java
@@ -17,7 +17,7 @@ public class Test012_3_02_AddTestData_300 {
 		for(int i = 0; i < 300; i++) {
 			String subject = String.format("테스트 제목입니다:[%03d]", i);
 			String content = String.format("내용무:[%03d]", i);
-			this.questionService.create(subject, content);
+			//this.questionService.create(subject, content);	// not work after 3.08, added SiteUser author @ Question and Answer class
 		}
 	}
 }
