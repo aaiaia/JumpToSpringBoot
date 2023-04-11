6dc5bd11047cee13bbced8865dc38aff34ce409b
PleYa <aaiaia.gg@gmail.com>
2023-04-12 03:26:10 +0900

    [WIP] 3.08, fixed createAnswer(), add arg Principal to get authenticated user name and find user(objects)

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -12,9 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 
+/* To Get user(spring security) */
+import java.security.Principal;
+
 import com.mysite.sbb.question.Question;
 //import com.mysite.sbb.question.QuestionForm;
 import com.mysite.sbb.question.QuestionService;
+
+/* for SiteUser(author) */
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.SiteUserService;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -23,19 +30,21 @@ public class AnswerController {
 
 	private final QuestionService questionService;
 	private final AnswerService answerService;
+	private final SiteUserService siteUserService;
 
 	@PostMapping("/create/{id}")
 	/*
 	 * previous method declare
 	public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
 	*/
-	public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
-		System.out.println("called: " + new Object(){}.getClass().getEnclosingMethod().getName() + ", in " + this.getClass().getName());
+	public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
+		//System.out.println("called: " + new Object(){}.getClass().getEnclosingMethod().getName() + ", in " + this.getClass().getName());
 
 		Question question = this.questionService.getQuestion(id);
+		SiteUser siteUser = this.siteUserService.getUser(principal.getName());
 		if(!bindingResult.hasErrors()) {
 			// TODO: 답변을 저장한다.
-			answerService.create(question, answerForm.getContent());
+			answerService.create(question, answerForm.getContent(), siteUser);
 			/* Return to Browser, createAnswer() called only(fixed) @url: /question/detail/{id} */
 			return String.format("redirect:/question/detail/%s", id);
 		} else {
