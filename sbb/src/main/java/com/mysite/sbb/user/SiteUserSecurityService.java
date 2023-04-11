a101a2c38218eb80f482521036bfb64108e5d81d
PleYa <aaiaia.gg@gmail.com>
2023-04-12 00:55:15 +0900

    [WIP] 3.07, implementated SiteUserSecurityService.java (2)

--- sbb/src/main/java/com/mysite/sbb/user/SiteUserSecurityService.java
+++ sbb/src/main/java/com/mysite/sbb/user/SiteUserSecurityService.java
@@ -12,10 +12,31 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.mysite.sbb.UserRole;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
-public class SiteUserSecurityService {
+public class SiteUserSecurityService implements UserDetailsService{
+
+	private final SiteUserRepository siteUserRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<SiteUser> _siteUser = this.siteUserRepository.findByUsername(username);
+		if(_siteUser.isEmpty()) {
+			throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+		}
+		SiteUser siteUser = _siteUser.get();
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		if("admin".equals(username)) {
+			authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
+		}
+		else {
+			authorities.add(new SimpleGrantedAuthority(UserRole.USER.getValue()));
+		}
+		return new User(siteUser.getUsername(), siteUser.getPassword(), authorities);
+	}
 
 }
