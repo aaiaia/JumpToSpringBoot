0904f9e9bcd90ac3049f526428f3eb2449783a2e
PleYa <aaiaia.gg@gmail.com>
2023-04-17 17:12:26 +0900

    3.10, added feature deleting answer

--- sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -88,4 +88,15 @@ public class AnswerController {
 		this.answerService.modify(answer, answerForm.getContent());
 		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
 	}
+
+	@GetMapping("/delete/{id}")
+	@PreAuthorize("isAuthenicated")
+	public String deleteAnswer(@PathVariable("id") Integer id, Principal principal) {
+		Answer answer = this.answerService.getAnswer(id);
+		if(!answer.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+		}
+		this.answerService.delete(answer);
+		return String.format("rediret:/question/detail/%s", answer.getQuestion().getId());
+	}
 }
--- sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -45,4 +45,8 @@ public class AnswerService {
 		answer.setModifiedDate(LocalDateTime.now());
 		this.answerRepository.save(answer);
 	}
+
+	public void delete(Answer answer) {
+		this.answerRepository.delete(answer);
+	}
 }
